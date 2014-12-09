fc1ca09692923181ff4244b11b7fc1c81cbb6531
Raymond Dodge <git@rayrobdod.name>
2014-12-09 01:55:59 -0500

    Adding events corresponding to Granny and her reststop

--- pkmn-js/gen6.1.js
+++ pkmn-js/gen6.1.js
@@ -9,4 +9,35 @@
 	} else {
 		showGameClock("TPP ORAS Game Clock<br/>", date);
 	}
-})(); 
+})();
+
+
+// Granny
+addEvent(new Building({
+	skipme: true,
+	name: "RestStop",
+	sprite: "img/bld/reststop.png",
+	x: -12, y: 21,
+	
+	warp_x: 16, warp_y: 48,
+}));
+
+addEvent(new Person({
+	skipme: true,
+	name : "Granny",
+	spritesheet : "img/trainers/oldlady.png",
+	x: -11, y: 22,
+	
+	activeZone: {
+		left: 22, right: -10,
+		top: 22, bottom: -10,
+	},
+	
+	behavior: behavior.look,
+	
+	dialog : [
+		"Oh dear, oh dear. <br/> Are your POK&eacute;MON still tired?",
+		"You should take another rest here. <br/> That's a fine idea. You should do that.",
+		"That's right. Take your time and rest up.",
+	],
+}));
